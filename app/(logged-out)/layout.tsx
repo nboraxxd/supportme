cfe99b49f907d377aec3928e0b9d1508ee133bba
bora <nhatbaoxxd@gmail.com>
2024-03-20 17:11:23 +0700

    Add UI elements and improve layout on the home page

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,3 @@
+export default function LoggedOut({ children }: { children: React.ReactNode }) {
+  return <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">{children}</main>
+}
--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,3 +1,25 @@
+import { PersonStandingIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>page</div>
+  return (
+    <>
+      <h1 className="flex items-center gap-2">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        SupportMe
+      </h1>
+      <p>The best dashboard to manage customer support</p>
+      <div className="flex items-center gap-2">
+        <Button asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button variant="outline" asChild>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+    </>
+  )
 }
